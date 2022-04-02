1e9c7b539c0fd98fa692f5fe312dd1abfa95fd21
Kalabaz31 <zahzouhy@gmail.com>
2022-04-02 07:18:26 +0100

    Initial Commit

---
+++ backend/schemas/schema.js
@@ -0,0 +1,20 @@
+import createSchema from "part:@sanity/base/schema-creator";
+
+import schemaTypes from "all:part:@sanity/base/schema-type";
+
+import works from "./works";
+import abouts from "./abouts";
+import contact from "./contact";
+
+export default createSchema({
+  // We name our schema
+  name: "default",
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    /* Your types here! */
+    works,
+    abouts,
+    contact,
+  ]),
+});
